b92778330ac8ec8f2573f6226f78217f07f3a356
Dwayne <104311388+davisdw@users.noreply.github.com>
2023-11-27 18:37:25 -0500

    Final Save work in progress

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -37,3 +37,15 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome_page():
+    return( f"<p>Welcome to the Hawaii Weather!! Surf's UP!!</p><br/>"
+            f"<p>Below is your Navigation Guide</p><br/><br/>"
+            f"/api/v1.0/precipitation<br/>Returns a JSON list of percipitation data for the dates between 8/23/16 and 8/23/17<br/><br/>"
+            f"/api/v1.0/stations<br/>Returns a JSON list of the weather stations<br/><br/>"
+            f"/api/v1.0/tobs<br/>Returns a JSON list of the Temperature Observations (tobs) for each station for the dates between 8/23/16 and 8/23/17<br/><br/>"
+            f"/api/v1.0/date<br/>Returns a JSON list of the minimum temperature, the average temperature, and the max temperature for the dates between the given start date and 8/23/17<br/><br/>."
+            f"/api/v1.0/start_date/end_date<br/>Returns a JSON list of the minimum temperature, the average temperature, and the max temperature for the dates between the given start date and end date<br/><br/>."
+        )
+    
